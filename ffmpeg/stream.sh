5ed55b0e9b392f8153f285f99366076db7e4667a
c-noblok <jacob.a.noble@gmail.com>
2020-08-03 03:11:55 -0400

    working dockerfiles

--- ffmpeg/stream.sh
+++ ffmpeg/stream.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+sleep 2
 
 {
 ffmpeg -f alsa -i plughw:2,0 \
 		-f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 \
-		-f rtsp rtsp://rtsp:${RSTP_HOST_PORT}${RSTP_HOST_PATH}
+		-f rtsp rtsp://simple-rtsp:${RSTP_HOST_PORT}${RSTP_HOST_PATH}
 	} ||
 {
 ffmpeg -f alsa -i plughw:1,0 \
 		-f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 \
-		-f rtsp rtsp://rtsp:${RSTP_HOST_PORT}${RSTP_HOST_PATH}
+		-f rtsp rtsp://simple-rtsp:${RSTP_HOST_PORT}${RSTP_HOST_PATH}
 	} ||
 {
 ffmpeg \
 		-f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 \
-		-f rtsp rtsp://rtsp:${RSTP_HOST_PORT}${RSTP_HOST_PATH}
+		-f rtsp rtsp://simple-rtsp:${RSTP_HOST_PORT}${RSTP_HOST_PATH}
 	}
--- vlc/vlc_http_stream.sh
+++ vlc/vlc_http_stream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-vlc --intf dummy -vvv "rtsp://rtsp:$rtsp_host/picam" --sout \
+sleep 10
+cvlc -vvv rtsp://simple-rtsp:8554/picam --sout \
                 "#transcode{vcodec=theo,vb=800,acodec=vorb,ab=128,\
-		channels=2,samplerate=44100,scodec=none}:http{mux=ogg,dst=:r$HTTP_STREAM_PORT/picam}"
+		channels=2,samplerate=44100,scodec=none}:http{mux=ogg,dst=:r8080/picam}"
